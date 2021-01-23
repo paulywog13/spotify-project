c29901aaf211bf81c564ccd0bb4945c903cf9a57
Stuart Yates <sjwyates@Mac-mini.local>
2021-01-23 13:40:01 -0700

    set up directory structure, pipenv

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+from sassutils.wsgi import SassMiddleware
+
+app = Flask(__name__)
+
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/scss', 'static/css', '/static/css')
+})
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/data")
+def get_data():
+    return 'this is where I give you the data'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
