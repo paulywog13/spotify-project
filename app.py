524347d65044fd7a832e08927a06f535b1a30d58
Stuart Yates <sjwyates@Mac-mini.local>
2021-01-27 20:53:25 -0700

    mongo

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, render_template, json
 from sassutils.wsgi import SassMiddleware
+from flask_pymongo import PyMongo
+import json
 import pandas as pd
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["MONGO_URI"] = "mongodb://localhost:27017/spotify-db"
+mongo = PyMongo(app)
 
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
     'app': ('static/scss', 'static/css', '/static/css')
@@ -16,10 +21,16 @@ def home():
 
 @app.route("/data")
 def get_data():
-    df = pd.csv('spotify.csv')
-    df_json = df.to_json(orient="index")
-    res =
-    return 'this is where I give you the data'
+    # run playlist.py (gets playlist from spotify API)
+    # get the snapshot_id from the response
+    # current_snapshot_id = mongo.db.playlist.find_one()
+    # if (api_snapshot_id != current_snapshot_id):
+        # run the code in audio-features.py
+        # update tracks collection in mongo
+        # run the code in user.py
+        # update user in mongo
+    tracks = mongo.db.tracks.find()
+    return json.dumps(tracks)
 
 
 if __name__ == "__main__":
---
+++ static/js/app.js
@@ -0,0 +1,1 @@
+d3.json('http://127.0.0.1:5000/data', res => console.log(res))
